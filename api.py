d6ff2d6ee9bc8cb68d5684128ea8f2dc7328beeb
Ryan McCants <ryan.mccants@gmail.com>
2016-08-10 07:08:30 -0500

    refactor that will probably be refactored

--- api.py
+++ api.py
@@ -69,20 +69,4 @@ def create_item(item, store_id):
     r = requests.post(url, headers=headers, json=item)
 
 
-def batch(reqs):
-    '''
-    Takes a list of requests in json
-    '''
-    batch_url = rooturl + '/batch'
-    resp = []
-    for sublist in [reqs[i:i+30] for i in range(0, len(reqs), 30)]:
-        batch_req = {'requests': []}
-        for req in sublist:
-            batch_req['requests'].append(req)
-        try:
-            r = requests.post(batch_url, json=batch_req)
-        except requests.exceptions.RequestException as e:
-            print(e)
-        else:
-            resp += r.json()
-    return resp
+
